e6c0372fe4234d2ffed67e2cbf108f38e83bf9ce
Alopez048 <73678386+Alopez048@users.noreply.github.com>
2020-11-06 06:43:37 -0600

    Se agregó un nuevo panel

--- src/Code/Bebidas.java
+++ src/Code/Bebidas.java
@@ -36,40 +36,4 @@ public class Bebidas {
         catch(SQLException e) {
         System.err.println("No se pudo guardar"+ e.getMessage());
         }
-    }
-        
- /*public void ModificarDatos(int ID_EMPLEADO, String PRIMER_NOMBRE, 
-        String PRIMER_APELLIDO,String SEGUNDO_NOMBRE,
-        String SEGUNDO_APELLIDO,String NIT,int SALARIO,int ESTATUS,int ID_DEPARTAMENTO){
-     try{
-            
-         Connection empresa= nuevo.getConection();
-         CallableStatement CS=  empresa.prepareCall("call modificarMEmpleado (?,?,?,?,?,?,?,?,?)");
-        CS.setInt(1,ID_EMPLEADO);
-        CS.setString(2,PRIMER_NOMBRE );
-        CS.setString(3,PRIMER_APELLIDO );
-        CS.setString(4,SEGUNDO_NOMBRE );
-        CS.setString(5,SEGUNDO_APELLIDO );
-        CS.setString(6,NIT );
-        CS.setInt(7,SALARIO );
-        CS.setInt(8, ESTATUS);
-        CS.setInt(9, ID_DEPARTAMENTO);
-        CS.executeUpdate();
-           
-            JOptionPane.showMessageDialog(null, "Se Guardaron los cambios");
-        }
-        
-        catch(SQLException e){
-        System.err.println("Hubo un erro al guardar"+e.getMessage());
-        }
-    }*/
-    
-    }
-    
-    
-    
-    
-    
-    
-    
-    
+    }         }
